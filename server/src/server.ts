fa5dcc16114ca261ec08a376b5a3a9b875bd96df
Alexis Menendez <alexis.menendez@austincc.edu>
2025-05-04 22:22:23 -0500

    trying to fix render build error

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,12 @@
 
 import dotenv from 'dotenv'; dotenv.config();
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import express from 'express';
 import path from 'node:path';
 import { ApolloServer } from '@apollo/server';
